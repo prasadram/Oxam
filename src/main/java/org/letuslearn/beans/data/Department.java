41d90d5beb1fef17171e9d78a22d7941c2ac0d83
jayavardhanvny <jayavardhanvny@gmail.com>
2017-05-11 22:24:29 +0530

    Modified

---
+++ src/main/java/org/letuslearn/beans/data/Department.java
@@ -0,0 +1,29 @@
+// Copyright (c) 2017-2018 LetUs Learn Inc.
+package org.letuslearn.beans.data;
+
+/**
+ * 
+ * @author aksharaaaa Department class.
+ *
+ */
+public class Department {
+  private int departmentId;
+  private String departmentName;
+
+  public int getDepartmentId() {
+    return departmentId;
+  }
+
+  public void setDepartmentId(int departmentId) {
+    this.departmentId = departmentId;
+  }
+
+  public String getDepartmentName() {
+    return departmentName;
+  }
+
+  public void setDepartmentName(String departmentName) {
+    this.departmentName = departmentName;
+  }
+
+}
